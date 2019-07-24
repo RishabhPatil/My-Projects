81f3b367ce82138b95cf4dcb6b4eb46ae623eeaf
snowham <51470282+snowham@users.noreply.github.com>
2019-07-24 21:57:49 +0530

    Update ClassesAndObjects.py

--- ClassesAndObjects.py
+++ ClassesAndObjects.py
@@ -3,23 +3,26 @@ class Robot:
     self.name = name
     self.color = color
     self.weight = weight
-  def introduce_self(self):
+  def introduceSelf(self):
     print("My name is " + self.name)
 
-# WITHOUT __init__ :
-# r1 = Robot()
-# r1.name = "Tom"
-# r1.color = "red"
-# r1.weight = 30
-# r2 = Robot()
-# r2.name = "Jerry"
-# r2.color = "blue"
-# r2.weight = 40
-# r1.introduce_self()
-# r2.introduce_self()
-
-# WITH __init__ :
 r1 = Robot("Tom", "red", 30)
 r2 = Robot("Jerry", "blue", 40)
-r1.introduce_self()
-r2.introduce_self()
+
+
+class Person:
+  def __init__(self, name, personality, isSitting, robotOwned):
+    self.name = name
+    self.personality = personality
+    self.isSitting = isSitting
+    self.robotOwned = robotOwned
+  def sitDown(self):
+    self.isSitting = True
+  def standUp(self):
+    self.isSitting = False
+
+p1 = Person("Alice", "aggresive", False, r1)
+p2 = Person("Becky", "talkative", True, r2)
+
+p1.robotOwned.introduceSelf()
+p2.robotOwned.introduceSelf()
